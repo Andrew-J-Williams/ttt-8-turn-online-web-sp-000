5d88945466c5874d35fb5bd9f59c38397de86761
Andrew Williams <1williamsaj@gmail.com>
2019-08-22 01:41:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,7 +28,7 @@ def valid_move?(board, index)
   array[index] = value #Takes our array with empty spaces, selects a value, and sets that value equal to the user character.
 end
 
-def turn(user_input)
+def turn(board)
   puts "Please enter 1-9:"
 
 end
